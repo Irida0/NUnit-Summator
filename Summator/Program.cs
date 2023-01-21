a7e3f32f99b0646ff9d7d7ba35e40ecdad508a7c
Irka <Irka@DESKTOP-1SAH47C>
2023-01-21 14:28:41 +0200

    Add project files.

---
+++ Summator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Summator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "The sum is: " + Summator.Sum (new int[] {1, 2, 3}));
+        }
+    }
+}
